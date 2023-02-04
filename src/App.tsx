a8329dc6da4bebe48ee7f40e2ee749ffdea16920
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-04 11:12:55 +1100

    Host site with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const App = () => <BrowserRouter><Routes><Route path='/' element={<b>Hi</b>} /></Routes></BrowserRouter>
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<b className='text-3xl'>Hi</b>} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
